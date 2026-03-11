8d0d91b1eede9885215664ad8e7368d0edabc102
Daniel <0804daniell@gmail.com>
2026-03-11 12:32:19 -0400

    6 franelas agregadas

--- src/mockData/products.js
+++ src/mockData/products.js
@@ -1,6 +1,12 @@
 import shirtCorazon from "../assets/shirts/corazon.png"
 import shirtLuna from '../assets/shirts/ad-astra-per-aspera.png'
 import shirtKintsugi from '../assets/shirts/kintsugi.png'
+import shirt1 from '../assets/shirts/shirt-1.png'
+import shirt2 from '../assets/shirts/shirt-2.png'
+import shirt3 from '../assets/shirts/shirt-3.png'
+import shirt4 from '../assets/shirts/shirt-4.png'
+import shirt5 from '../assets/shirts/shirt-5.png'
+import shirt6 from '../assets/shirts/shirt-6.png'
 
 export const products = [
             {
@@ -26,5 +32,41 @@ export const products = [
                 title: 'Fuí, soy, seré',
                 description: 'Para los que quieren una vida de disciplina, metas y exito',
                 img: shirtCorazon
+            },
+            {
+                id: 5,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt1
+            },
+            {
+                id: 6,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt2
+            },
+            {
+                id: 7,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt3
+            },
+            {
+                id: 8,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt4
+            },
+            {
+                id: 9,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt5
+            },
+            {
+                id: 10,
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra',
+                img: shirt6
             },
         ]
